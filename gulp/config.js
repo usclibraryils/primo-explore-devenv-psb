1e00a33afbf43ac415a325bb84c708f55a868976
Kun Lin <kunlin@usc.edu>
2022-11-04 09:35:13 -0700

    Update to current PSB view

--- gulp/config.js
+++ gulp/config.js
@@ -166,7 +166,7 @@ var SERVERS = {
  *   var PROXY_SERVER = 'http://abc-primo.hosted.exlibrisgroup.com'
  *   var PROXY_SERVER = 'https://abc-primo.hosted.exlibrisgroup.com:443'
  */
-var PROXY_SERVER = 'https://uosc.primo.exlibrisgroup.com:443';
+var PROXY_SERVER = 'https://uosc-psb.primo.exlibrisgroup.com:443';
 
 
 let buildParams = {
