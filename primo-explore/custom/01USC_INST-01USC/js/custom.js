2429734f8e8fd0cb82ff16c8d320ca16439e4dae
Kun Lin <kunlin@usc.edu>
2024-02-06 15:18:12 -0800

    sandbox update

--- primo-explore/custom/01USC_INST-01USC/js/custom.js
+++ primo-explore/custom/01USC_INST-01USC/js/custom.js
@@ -143,7 +143,7 @@ app.component('prmLoanAfter', {
 
   /*Add Report a Problem Link below Send To*/
   app.component('prmActionListAfter', {
-    template: '<br><a href="https://primotest-usc-lib1.pantheonsite.io/form/need-help-report-problem" referrerpolicy="no-referrer-when-downgrade" target="_blank" style="float:right;" class="md-primoExplore-theme"><b>Need Help? Report a Problem</b></a>'
+    template: '<br><a href="https://libraries.usc.edu/form/need-help-report-problem" referrerpolicy="no-referrer-when-downgrade" target="_blank" style="float:right;" class="md-primoExplore-theme"><b>Need Help? Report a Problem</b></a>'
   });
   /*End of Add Report a Problem Link below Send To*/
 })();
@@ -175,12 +175,21 @@ app.component('prmLoanAfter', {
     gtag('config', 'G-CMGZ809JXT');`;
     document.head.appendChild(googleAnalyticsCode); 
 
-
-    //END - GA4
+  
   })(); 
-//GA KEY INDICATOR
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NKSVLSB');
+  //END - GA4
+   //accessibility notes
+   (function () {
+   var buttondiv = document.createElement("div");
+   buttondiv.innerHTML = '<a style="color: rgb(255, 255, 255);display: block;position: absolute;text-align: center;border-width: 0; border-style: solid;" href="https://accessibility.usc.edu/accessibility-at-usc/digital-accessibility/" target="_blank">USC Digital Accessibility</a>';
+   buttondiv.style = "position:fixed;padding: 0.5em; width:11em;height:2.5em; bottom:0; right:2em; border-color: rgb(255, 255, 255);color: rgb(255, 255, 255);background-color: rgb(153, 0, 0);box-shadow: rgb(204, 204, 204) 0px 0px 5px;border-width: 4px 4px 0px"
+   document.body.appendChild(buttondiv);
+   })(); 
+   //end of accessibility notes
+
+    //GA KEY INDICATOR
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-NKSVLSB');
